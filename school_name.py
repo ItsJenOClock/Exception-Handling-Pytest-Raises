a564048446c6d60bd6d5b756cc58b2ee85acac84
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-02-10 00:08:22 -0600

    updates

--- school_name.py
+++ school_name.py
@@ -2,4 +2,10 @@ def claim_unreserved_code_school_name(name):
     if name == "Ada Developers Academy":
         raise ValueError("There's already an awesome school with that name!")
 
+    if name != "Ada Developers Academy":
+        raise ValueError("You didn't enter the right name")
+    
     return True
+
+claim_unreserved_code_school_name("")
+claim_unreserved_code_school_name("Ada Developers Academy")
--- tests/test_school_name_no_pytest_raises.py
+++ tests/test_school_name_no_pytest_raises.py
@@ -9,4 +9,23 @@ def test_invalid_name_no_pytest():
     # Think about what the test actually does and think about how to write them ourselves.
 
     # Replace this with your own logic
-    raise Exception("test not implemented")
+    try:
+        claim_unreserved_code_school_name("University of Washington")
+        assert ValueError, "Expected 'Ada Developers Academy'"
+
+    except ValueError as error:
+        print(f"{error}")
+
+# test_invalid_name_no_pytest()
+    # raise Exception("test not implemented")
+
+#     def test_divide():
+#     try:
+#         divide(10, 0)
+#         # This should not happen, so we raise an AssertionError
+#         assert False, "Expected ZeroDivisionError but no exception was raised"
+#     except ZeroDivisionError:
+#         # This is the expected behavior, so the test passes
+#         pass 
+
+# test_divide()
